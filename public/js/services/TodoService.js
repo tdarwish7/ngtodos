803ec2542127d815aa8aa486a80993b6ca6e0d9c
Teacher <Teacher>
2016-07-12 20:07:10 -0400

    finished dataservice

--- public/js/services/TodoService.js
+++ public/js/services/TodoService.js
@@ -7,6 +7,7 @@
   TodoService.$inject = ['$http'];
 
   function TodoService($http){
+    var baseUrl = 'https://quiet-refuge-27140.herokuapp.com/';
     var o = {
       create: createTodo, //function
       readAll: getAll, //function
@@ -16,14 +17,33 @@
     };
     return o;
 
-    function createTodo(){}
+    function createTodo(desc){
+      var info = {
+        description: desc
+      };
+      return $http.post(baseUrl+'todos', info)
+                  .then(function(response){
+                    getAll();
+                  });
+    }
     function getAll(){
-      return $http.get('https://quiet-refuge-27140.herokuapp.com/todos')
+      return $http.get(baseUrl + 'todos')
                   .then(function(response){
                     o.todos = response.data;
                   });
     }
-    function updateTodo(){}
-    function deleteTodo(){}
+
+    function updateTodo(id, newTodo){
+      return $http.put(baseUrl+'todos/'+id, newTodo)
+                  .then(function(response){
+                    getAll();
+                  });
+    }
+    function deleteTodo(id){
+      return $http.delete(baseUrl+'todos/'+id)
+                  .then(function(response){
+                    getAll();
+                  })
+    }
   }
 })()
